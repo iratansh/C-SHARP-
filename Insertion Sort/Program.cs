a2562b84cd0edd08df63543582cd8c2b64b8bdff
iratansh <151393106+iratansh@users.noreply.github.com>
2024-01-05 17:26:02 -0700

    Add files via upload

---
+++ Insertion Sort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program {
+    static void Main(string[] args) {
+        List<int> nums =  new List<int>{10, 70, 30, 100, 40, 45, 90, 80, 85};
+        List<string> words = new List<string>{"dog","at", "good", "eye", "cat", "ball", "fish"};
+
+        insertionSort(nums);
+        insertionSort(words);
+
+        foreach (var num in nums) {
+            Console.WriteLine(num);
+        } 
+
+        foreach (var el in words) {
+            Console.WriteLine(el);
+        }
+    }
+
+    static void insertionSort<T>(List<T> list) {
+        for (int i = 0; i < list.Count; i++) {
+            var currVal = list[i];
+            var currPos = i - 1;
+
+            while (currPos  >= 0 && Comparer<T>.Default.Compare(list[currPos], currVal) > 0) {
+                list[currPos + 1] = list[currPos];
+                currPos -= 1;
+
+            }
+
+            list[currPos + 1] = currVal;
+        }
+
+    }
+}
---
+++ Linear Search/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program {
+    
+    static void Main(string[] args) {
+        List<int> nums = new List<int>{10, 30, 40, 45, 70, 80, 85, 90, 100};
+        List<string> words = new List<string>{"at", "ball", "cat", "dog", "eye", "fish", "good"};
+        List<int> unsorted = new List<int>{30, 20, 70, 40, 50, 100, 90};
+
+        Console.WriteLine(linearSearch(nums, 70));
+        Console.WriteLine(linearSearch(words, "cat"));
+        Console.WriteLine(linearSearch(unsorted, 100));
+
+
+    }
+
+    static int linearSearch<T>(List<T> list, T item) {
+        for (int i = 0; i < list.Count; i++) {
+            if (EqualityComparer<T>.Default.Equals(list[i], item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+        
+
+}
+
